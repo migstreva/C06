aaa96fa1598f5620f4fc28dc85abc2549a188ac4
migstreva <128249230+migstreva@users.noreply.github.com>
2025-02-28 22:28:12 -0300

    Delete Exercicio6_MarioKart directory

--- Exercicio6_MarioKart/src/Motor.java
+++
@@ -1,9 +0,0 @@
-public class Motor {
-    String cilindradas;
-    float velocidadeMaxima;
-
-    void mostraInfo(){
-        System.out.println("Velocidade: " + velocidadeMaxima);
-        System.out.println("Cilindradas: " + cilindradas);
-    }
-}
--- Exercicio6_MarioKart/src/Piloto.java
+++
@@ -1,8 +0,0 @@
-public class Piloto {
-    String nome;
-    boolean vilao;
-
-    void soltaSuperPoder(){
-        System.out.println(nome + " soltando super poder...");
-    }
-}
