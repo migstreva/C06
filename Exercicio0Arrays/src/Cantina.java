c1dbbbe7a10462d7f6a8fce89703af26b62eebff
migstreva <128249230+migstreva@users.noreply.github.com>
2025-03-30 02:51:24 -0300

    Add files via upload

---
+++ Exercicio0Arrays/src/Cantina.java
@@ -0,0 +1,25 @@
+public class Cantina {
+    String nome;
+    Salgado[] salgados = new Salgado[3];
+
+    void addSalgado(Salgado novoSalgado) {
+        boolean added = false; //Verifica se foi possivel adicionar o salgado
+        for(int i = 0; i < salgados.length; i++){
+            if(salgados[i] == null){
+                salgados[i] = novoSalgado;
+                added = true;
+                break;
+            }
+        }
+        if(!added){
+            System.out.println("Nao foi possivel adicionar salgado.");
+        }
+    }
+
+    void mostraInfo(){
+        System.out.println("Lista de salgados:");
+        for(Salgado salgado : salgados){
+            System.out.println(salgado);
+        }
+    }
+}
