f3ad211e34a162bf6517b22be7e8c10e2ddc4a7c
migstreva <128249230+migstreva@users.noreply.github.com>
2025-04-04 18:03:38 -0300

    Add files via upload

---
+++ PCMania/src/Cliente.java
@@ -0,0 +1,20 @@
+public class Cliente {
+    String nome;
+    long cpf;
+    float totalCompra = 0;
+
+    public Cliente(String nome, long cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    float calculaTotalCompra(Computador[] cart){
+        for (Computador computador : cart) {
+            if(computador != null) {
+                totalCompra += computador.preco;
+            }
+        }
+        return totalCompra;
+    }
+
+}
